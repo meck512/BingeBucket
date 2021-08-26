3606072de97fd921de5711f5a24bb574b6e74513
meck512 <m_eck12@outlook.com>
2021-08-26 17:46:48 -0400

    added basics of Feature and Review models

---
+++ models/Feature.js
@@ -0,0 +1,41 @@
+// Once we decide what info we want from the api we will have to add things like movie poster, actors, imbd rating etc.
+
+
+
+Feature.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      feature_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isURL: true
+        }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'Feature'
+    }
+  );
+  
+  module.exports = Feature;
+  
