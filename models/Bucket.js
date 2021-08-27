9c522167d63391f5d3c976c7b41e89535e978bcf
meck512 <m_eck12@outlook.com>
2021-08-26 20:30:19 -0400

    models potentially done

---
+++ models/Bucket.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Bucket.init(
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    feature_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'feature',
+        key: 'id'
+      }
+    },
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Review'
+  }
+);
+
+module.exports = Bucket;
