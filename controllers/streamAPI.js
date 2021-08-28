a7f82adfde0964c7a9a65a41ac6cc4f70e66ed4b
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-08-27 20:38:59 -0400

    add async await logic in streamAPI and user-routes

--- controllers/streamAPI.js
+++ controllers/streamAPI.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
 var axios = require("axios").default;
-// var singleItem;
 
-function getStreamList() {
+//Get an array of streaming series/movies
+//servicesIn = REQUIRED one or more of the following:
+//'netflix,prime,disney,hbo,hulu,peacock,paramount,starz,showtime,apple,mubi'
+//typeIn = REQUIRED	'movie' OR 'series'
+//e.g. getStreamList('netflix,prime','movie')
+//e.g. getStreamList('netflix','series')
+const getStreamList = async function (servicesIn, typeIn) {
 	var options = {
 		method: 'GET',
 		url: 'https://streaming-availability.p.rapidapi.com/search/ultra',
 		params: {
 			country: 'us',
 			// services: 'netflix,prime,disney,hbo,hulu,peacock,paramount,starz,showtime,apple,mubi',
-			services: 'netflix',
-			type: 'movie',
+			services: servicesIn,
+			type: typeIn,
 			order_by: 'imdb_vote_count',
 			year_min: '2000',
 			year_max: '2020',
@@ -29,16 +34,21 @@ function getStreamList() {
 		}
 	};
 
-	axios.request(options).then(function (response) {
-		// console.log('total pages returned: ', response.data.total_pages);
-		// console.log(response.data.results);
-		return response.data.results;
-	}).catch(function (error) {
-		console.error(error);
-	});
+	const resultData = await axios.request(options);
+	return resultData.data.results;
+	// .then(function (response) {
+	// 	console.log('total pages returned: ', response.data.total_pages);
+	// 	// console.log(response.data.results);
+	// 	return response.data.results;
+	// }).catch(function (error) {
+		// console.error(error);
+	// });
 };
 
-function getItemData(id) {
+//Get the individual show from the API 
+// Usage id is TMDb ID or an IMDb in the format of tv/71712 or movie/464052
+//e.g. getItemData('tv/66732');
+async function getItemData(id) {
 	var options = {
 		method: 'GET',
 		url: 'https://streaming-availability.p.rapidapi.com/get/basic',
@@ -53,12 +63,15 @@ function getItemData(id) {
 		console.log(response.data);
 		console.log(response.data.streamingInfo);
 		// singleItem = response.data;
+		return response.data.results;
 	}).catch(function (error) {
 		console.error(error);
 	});
 };
+async function test() {
+	const data = await getStreamList('netflix', 'series');
+	console.log(data);
+};
+test();
 
-// getItemData('movie/120');
-
-getItemData('tv/66732');
-// getStreamList();
+exports.getStreamList = getStreamList;
