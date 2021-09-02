259695cde7ad581a2e43efc19120ac2627e03559
KevinPMyers <kmyers25@gmail.com>
2021-09-02 16:44:38 -0400

    commit to fix homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,45 +5,11 @@ const { User, Comment } = require('../models');
 router.get('/', (req, res) => {
   console.log('======================');
   res.render('homepage', {
-    // posts,
-    // loggedIn: req.session.loggedIn
+    
+    loggedIn: req.session.loggedIn
   });
 
-  // Post.findAll({
-  //   attributes: [
-  //     'id',
-  //     'post_url',
-  //     'title',
-  //     'created_at'
-
-  //   ],
-  //   include: [
-  //     {
-  //       model: Comment,
-  //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-  //       include: {
-  //         model: User,
-  //         attributes: ['username']
-  //       }
-  //     },
-  //     {
-  //       model: User,
-  //       attributes: ['username']
-  //     }
-  //   ]
-  // })
-  //   .then(dbPostData => {
-  //     const posts = dbPostData.map(post => post.get({ plain: true }));
-
-  //     res.render('homepage', {
-  //       posts,
-  //       loggedIn: req.session.loggedIn
-  //     });
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   });
+  
 });
 
 // get single post
