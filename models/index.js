9a87bfc83ad62d29a52d7cf11ee2444eef86a327
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-09-01 21:09:25 -0400

    index merge error

--- models/index.js
+++ models/index.js
@@ -3,11 +3,6 @@ const User = require('./User');
 const Feature = require('./Feature');
 const Comment = require('./Comment');
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> c18c44fdd10e6d55f114f8f1d32a97bd956e4f7c
 Feature.hasMany(Review, {
   foreignKey: 'review_id'
 });
@@ -18,10 +13,4 @@ Comment.belongsTo(Review, {
   foreignKey: 'review_id',
 });
 
-<<<<<<< HEAD
 module.exports = { User, Review,  Feature, Comment };
-=======
-
-module.exports = { User, Review,  Feature, Comment };
-
->>>>>>> c18c44fdd10e6d55f114f8f1d32a97bd956e4f7c
