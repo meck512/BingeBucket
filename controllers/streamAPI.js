c93adf152bd847862fa7ed1b2e1a18e4da0df0a3
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-09-03 19:14:40 -0400

    combine arrays

--- controllers/streamAPI.js
+++ controllers/streamAPI.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 var axios = require("axios").default;
 
 //Get an array of streaming series/movies
@@ -7,15 +7,15 @@ var axios = require("axios").default;
 //typeIn = REQUIRED	'movie' OR 'series'
 //e.g. getStreamList('netflix,prime','movie')
 //e.g. getStreamList('netflix','series')
-const getStreamList = async function (servicesIn, typeIn) {
-	var options = {
+const getStreamList = async function (servicesIn) {
+	var optionsMovie = {
 		method: 'GET',
 		url: 'https://streaming-availability.p.rapidapi.com/search/ultra',
 		params: {
 			country: 'us',
 			// services: 'netflix,prime,disney,hbo,hulu,peacock,paramount,starz,showtime,apple,mubi',
 			services: servicesIn,
-			type: typeIn,
+			type: 'movie',
 			order_by: 'imdb_vote_count',
 			year_min: '2000',
 			year_max: '2020',
@@ -30,46 +30,66 @@ const getStreamList = async function (servicesIn, typeIn) {
 		},
 		headers: {
 			'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
-			// 'x-rapidapi-key': process.env.STREAM_API_KEY
-			'x-rapidapi-key': "f2ee12cbffmsh9a043e6ff8eb403p15aba3jsn46870cf178d2"
+			'x-rapidapi-key': 'f2ee12cbffmsh9a043e6ff8eb403p15aba3jsn46870cf178d2'
 		}
 	};
 
-	const resultData = await axios.request(options);
-	return resultData.data.results;
-	// .then(function (response) {
-	// 	console.log('total pages returned: ', response.data.total_pages);
-	// 	// console.log(response.data.results);
-	// 	return response.data.results;
-	// }).catch(function (error) {
-	// console.error(error);
-	// });
+	var optionsSeries = {
+		method: 'GET',
+		url: 'https://streaming-availability.p.rapidapi.com/search/ultra',
+		params: {
+			country: 'us',
+			// services: 'netflix,prime,disney,hbo,hulu,peacock,paramount,starz,showtime,apple,mubi',
+			services: servicesIn,
+			type: 'series',
+			order_by: 'imdb_vote_count',
+			year_min: '2000',
+			year_max: '2020',
+			// page: '326',
+			// genres_relation: 'or',
+			desc: 'true',
+			language: 'en',
+			min_imdb_rating: '50',
+			max_imdb_rating: '100',
+			min_imdb_vote_count: '10000',
+			max_imdb_vote_count: '1000000'
+		},
+		headers: {
+			'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
+			'x-rapidapi-key': 'f2ee12cbffmsh9a043e6ff8eb403p15aba3jsn46870cf178d2'
+		}
+	};
+
+	const resultDataMovie = await axios.request(optionsMovie);
+	const resultDataSeries = await axios.request(optionsSeries);
+	const resultDataMovieArr = resultDataMovie.data.results;
+	const resultDataSeriesArr = resultDataSeries.data.results;
+
+	returnCombinedData = resultDataSeriesArr.concat(resultDataMovieArr);
+
+	console.log(returnCombinedData);
+	return returnCombinedData;
 };
 
 //Get the individual show from the API 
 // Usage id is TMDb ID or an IMDb in the format of tv/71712 or movie/464052
 //e.g. getItemData('tv/66732');
-async function getItemData(id) {
+const getItemData = async function (id) {
 	var options = {
 		method: 'GET',
 		url: 'https://streaming-availability.p.rapidapi.com/get/basic',
-		params: { country: 'us', tmdb_id: id },
+		params: { tmdb_id: id },
 		headers: {
 			'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
-			// 'x-rapidapi-key': process.env.STREAM_API_KEY
-			'x-rapidapi-key': "f2ee12cbffmsh9a043e6ff8eb403p15aba3jsn46870cf178d2"
+			'x-rapidapi-key': process.env.STREAM_API_KEY
 		}
 	};
 
-	axios.request(options).then(function (response) {
-		// console.log(response.data);
-		// console.log(response.data.streamingInfo);
-		// singleItem = response.data;
-		return response.data.results;
-	}).catch(function (error) {
-		console.error(error);
-	});
+	const resultData = await axios.request(options);
+	return resultData.data.results;
 };
 
 
-exports.getStreamList = getStreamList;
+exports.getStreamList = { getStreamList, getItemData };
+
+console.log(getStreamList('netflix'));
