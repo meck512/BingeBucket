55238b04011ec502a77a5ccc7a42060efb82352d
meck512 <m_eck12@outlook.com>
2021-09-04 11:11:41 -0400

    update layout

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const Review = require('./Review');
+const User = require('./User');
+const Feature = require('./Feature');
+const Comment = require('./Comment');
+
+Feature.hasMany(Review, {
+  foreignKey: 'review_id'
+});
+Review.hasMany(Comment, {
+  foreignKey: 'comment_id',
+});
+Comment.belongsTo(Review, {
+  foreignKey: 'review_id',
+});
+
+
+module.exports = { User, Review,  Feature, Comment };
+
